83f5db7389712c70d8bb054a88d0bd2cbef29440
Dave Carey <carvsdriver@gmail.com>
2013-11-12 18:50:01 -0800

    Implement ADB over network
    Change-Id: I02a7c556b345bf7e32d61e22c9b2d8cc0e49ce3d

--- Settings/src/com/android/tv/settings/system/development/DevelopmentFragment.java
+++ Settings/src/com/android/tv/settings/system/development/DevelopmentFragment.java
@@ -90,7 +90,12 @@ import com.android.tv.settings.system.development.audio.AudioDebug;
 import com.android.tv.settings.system.development.audio.AudioMetrics;
 import com.android.tv.settings.system.development.audio.AudioReaderException;
 
+import java.net.NetworkInterface;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -106,6 +111,8 @@ public class DevelopmentFragment extends SettingsPreferenceFragment
     private static final String ENABLE_DEVELOPER = "development_settings_enable";
     private static final String ENABLE_ADB = "enable_adb";
     private static final String ENABLE_ADB_ROOT = "enable_adb_root";
+    private static final String ADB_TCPIP = "adb_over_network";
+    private static final String ADB_PORT_PROP = "service.adb.tcp.port";
     private static final String CLEAR_ADB_KEYS = "clear_adb_keys";
     private static final String ENABLE_TERMINAL = "enable_terminal";
     private static final String KEEP_SCREEN_ON = "keep_screen_on";
@@ -204,6 +211,7 @@ public class DevelopmentFragment extends SettingsPreferenceFragment
 
     private TwoStatePreference mEnableDeveloper;
     private TwoStatePreference mEnableAdb;
+    private TwoStatePreference mAdbOverNetwork;
     private Preference mClearAdbKeys;
     private TwoStatePreference mEnableAdbRoot;
     private TwoStatePreference mEnableTerminal;
@@ -498,6 +506,12 @@ public class DevelopmentFragment extends SettingsPreferenceFragment
         } else {
             mWirelessDebugging.setFragment(WirelessDebuggingFragment.class.getName());
         }
+
+        mAdbOverNetwork = findAndInitSwitchPref(ADB_TCPIP);
+        if (mAdbOverNetwork != null) {
+            mWirelessDebugging.setVisible(false);
+            mWirelessDebugging = null;
+        }
     }
 
     private void removePreference(String key) {
@@ -732,6 +746,41 @@ public class DevelopmentFragment extends SettingsPreferenceFragment
         updateUSBAudioOptions();
         updateForceResizableOptions();
         updateAudioRecordingOptions();
+        updateAdbOverNetwork();
+    }
+
+    private void updateAdbOverNetwork() {
+        final int port = SystemProperties.getInt(ADB_PORT_PROP, -1);
+        final boolean enabled = port > 0;
+
+        updateSwitchPreference(mAdbOverNetwork, enabled);
+
+        String hostAddress = null;
+
+        if (enabled) {
+            try {
+                List<NetworkInterface> interfaces = Collections.list(
+                        NetworkInterface.getNetworkInterfaces());
+                for (NetworkInterface intf : interfaces) {
+                    List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                    for (InetAddress addr : addrs) {
+                        if (!addr.isLoopbackAddress() &&
+                            addr.getHostAddress().indexOf(':') < 0) {
+                            hostAddress = addr.getHostAddress();
+                            break;
+                        }
+                    }
+                }
+            } catch (SocketException se) {
+                // Do nothing
+            };
+        }
+
+        if (hostAddress != null) {
+            mAdbOverNetwork.setSummary(hostAddress + ":" + String.valueOf(port));
+        } else {
+            mAdbOverNetwork.setSummary(R.string.adb_over_network_summary);
+        }
     }
 
     private void resetDangerousOptions() {
@@ -1693,6 +1742,9 @@ public class DevelopmentFragment extends SettingsPreferenceFragment
             }
         } else if (preference == mEnableAdbRoot) {
             mADBRootService.setEnabled(mEnableAdbRoot.isChecked());
+        } else if (preference == mAdbOverNetwork) {
+            SystemProperties.set(ADB_PORT_PROP, mAdbOverNetwork.isChecked() ? "5555" : "-1");
+            updateAdbOverNetwork();
         } else if (preference == mEnableTerminal) {
             final PackageManager pm = getActivity().getPackageManager();
             pm.setApplicationEnabledSetting(TERMINAL_APP_PACKAGE,
