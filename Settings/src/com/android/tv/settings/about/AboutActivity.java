8659c8df6932d15d35ab178845a6855e1dc8bc92
dhacker29 <dhackerdvm@gmail.com>
2015-04-21 00:28:43 -0400

    Add build date (ro.build.date from build.prop) to About screen.
    Change-Id: I695e5a8b01616cddf865223d3b037f594da35224

--- Settings/src/com/android/tv/settings/about/AboutActivity.java
+++ Settings/src/com/android/tv/settings/about/AboutActivity.java
@@ -100,6 +100,13 @@ public class AboutActivity extends DialogActivity implements ActionAdapter.Liste
     }
 
     /**
+     * Get the build date.
+     */
+    public static String getBuildDate() {
+        return SystemProperties.get("ro.build.date");
+    }
+
+    /**
      * Intent component to launch PlatLogo Easter egg.
      */
     private static final ComponentName mPlatLogoActivity = new ComponentName("android",
@@ -276,6 +283,12 @@ public class AboutActivity extends DialogActivity implements ActionAdapter.Liste
                 .key("serial")
                 .title(getString(R.string.about_serial))
                 .description(Build.SERIAL)
+                .enabled(false)
+                .build());
+        actions.add(new Action.Builder()
+                .key("build_date")
+                .title(getString(R.string.about_build_date))
+                .description(getBuildDate())
                 .enabled(false)
                 .build());
         actions.add(new Action.Builder()
