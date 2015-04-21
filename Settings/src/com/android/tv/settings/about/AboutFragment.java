deab69cd7adea5d25267e76468941aad0127b441
dhacker29 <dhackerdvm@gmail.com>
2015-04-21 00:21:16 -0400

    One does not simply become a Developer
    Change-Id: I4e5f89496d40a04cbc6242d8580334f039d251f2

--- Settings/src/com/android/tv/settings/about/AboutFragment.java
+++ Settings/src/com/android/tv/settings/about/AboutFragment.java
@@ -355,7 +355,7 @@ public class AboutFragment extends SettingsPreferenceFragment {
                         if (mDevHitToast != null) {
                             mDevHitToast.cancel();
                         }
-                        mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_on,
+                        mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_on_cm,
                                 Toast.LENGTH_LONG);
                         mDevHitToast.show();
                         // This is good time to index the Developer Options
@@ -368,7 +368,7 @@ public class AboutFragment extends SettingsPreferenceFragment {
                             mDevHitToast.cancel();
                         }
                         MessageFormat msgFormat = new MessageFormat(
-                                getResources().getString(R.string.show_dev_countdown),
+                                getResources().getString(R.string.show_dev_countdown_cm),
                                 Locale.getDefault());
                         Map<String, Object> arguments = new HashMap<>();
                         arguments.put("count", mDevHitCountdown);
@@ -380,7 +380,7 @@ public class AboutFragment extends SettingsPreferenceFragment {
                     if (mDevHitToast != null) {
                         mDevHitToast.cancel();
                     }
-                    mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_already,
+                    mDevHitToast = Toast.makeText(getActivity(), R.string.show_dev_already_cm,
                             Toast.LENGTH_LONG);
                     mDevHitToast.show();
                 }
