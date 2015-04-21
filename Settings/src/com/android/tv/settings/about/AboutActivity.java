f0e8aecf46869c4928926111733a9a8897b02013
dhacker29 <dhackerdvm@gmail.com>
2015-04-21 00:59:39 -0400

    About: Show the kernel version
    Change-Id: Icb5059523b2064f43bf15c230cc3a983b4549c12

--- Settings/src/com/android/tv/settings/about/AboutActivity.java
+++ Settings/src/com/android/tv/settings/about/AboutActivity.java
@@ -43,8 +43,13 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Activity which shows the build / model / legal info / etc.
@@ -62,6 +67,8 @@ public class AboutActivity extends DialogActivity implements ActionAdapter.Liste
     private static final String KEY_VERSION = "version";
     private static final String KEY_REBOOT = "reboot";
     private static final String KEY_MOD_VERSION = "mod_version";
+    private static final String FILENAME_PROC_VERSION = "/proc/version";
+    private static final String LOG_TAG = "AboutSettings";
 
     /**
      * Intent action of SettingsLicenseActivity (for displaying open source licenses.)
@@ -104,6 +111,65 @@ public class AboutActivity extends DialogActivity implements ActionAdapter.Liste
      */
     public static String getBuildDate() {
         return SystemProperties.get("ro.build.date");
+    }
+
+    /**
+     * Reads a line from the specified file.
+     * @param filename the file to read from
+     * @return the first line, if any.
+     * @throws IOException if the file couldn't be read
+     */
+    private static String readLine(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename), 256);
+        try {
+            return reader.readLine();
+        } finally {
+            reader.close();
+        }
+    }
+
+    /**
+     * Get the kernel version.
+     */
+    public static String getFormattedKernelVersion() {
+        try {
+            return formatKernelVersion(readLine(FILENAME_PROC_VERSION));
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG,
+                "IO Exception when getting kernel version for Device Info screen",
+                e);
+
+            return "Unavailable";
+        }
+    }
+
+    public static String formatKernelVersion(String rawKernelVersion) {
+        // Example (see tests for more):
+        // Linux version 3.0.31-g6fb96c9 (android-build@xxx.xxx.xxx.xxx.com) \
+        //     (gcc version 4.6.x-xxx 20120106 (prerelease) (GCC) ) #1 SMP PREEMPT \
+        //     Thu Jun 28 11:02:39 PDT 2012
+
+        final String PROC_VERSION_REGEX =
+            "Linux version (\\S+) " + /* group 1: "3.0.31-g6fb96c9" */
+            "\\((\\S+?)\\) " +        /* group 2: "x@y.com" (kernel builder) */
+            "(?:\\(gcc.+? \\)) " +    /* ignore: GCC version information */
+            "(#\\d+) " +              /* group 3: "#1" */
+            "(?:.*?)?" +              /* ignore: optional SMP, PREEMPT, and any CONFIG_FLAGS */
+            "((Sun|Mon|Tue|Wed|Thu|Fri|Sat).+)"; /* group 4: "Thu Jun 28 11:02:39 PDT 2012" */
+
+        Matcher m = Pattern.compile(PROC_VERSION_REGEX).matcher(rawKernelVersion);
+        if (!m.matches()) {
+            Log.e(LOG_TAG, "Regex did not match on /proc/version: " + rawKernelVersion);
+            return "Unavailable";
+        } else if (m.groupCount() < 4) {
+            Log.e(LOG_TAG, "Regex match on /proc/version only returned " + m.groupCount()
+                    + " groups");
+            return "Unavailable";
+        }
+        return m.group(1) + "\n" +                 // 3.0.31-g6fb96c9
+            m.group(2) + " " + m.group(3) + "\n" + // x@y.com #1
+            m.group(4);                            // Thu Jun 28 11:02:39 PDT 2012
     }
 
     /**
@@ -283,6 +349,13 @@ public class AboutActivity extends DialogActivity implements ActionAdapter.Liste
                 .key("serial")
                 .title(getString(R.string.about_serial))
                 .description(Build.SERIAL)
+                .enabled(false)
+                .build());
+        actions.add(new Action.Builder()
+                .key("kernel_version")
+                .title(getString(R.string.about_kernel_version))
+                .description(getFormattedKernelVersion())
+                .multilineDescription(true)
                 .enabled(false)
                 .build());
         actions.add(new Action.Builder()
