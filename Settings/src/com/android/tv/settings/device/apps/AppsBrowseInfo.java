c7012256b566dad724d75b851bed3341c065ab12
Tony Mantler <nicoya@google.com>
2015-06-10 11:48:04 -0700

    Filter apps by volume
    Also coalesce updates to the apps screen to avoid jank and flicker
    
    b/21328967
    
    Change-Id: Ie77db8cdbc8a6ad81d8772fe4ddfd2deefc95840

--- Settings/src/com/android/tv/settings/device/apps/AppsBrowseInfo.java
+++ Settings/src/com/android/tv/settings/device/apps/AppsBrowseInfo.java
@@ -20,9 +20,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Intent.ShortcutIconResource;
+import android.content.pm.ApplicationInfo;
 import android.content.res.Resources;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v17.leanback.widget.ArrayObjectAdapter;
 import android.support.v17.leanback.widget.HeaderItem;
+import android.text.TextUtils;
+import android.util.ArrayMap;
 import android.util.Log;
 
 import com.android.settingslib.applications.ApplicationsState;
@@ -32,14 +37,15 @@ import com.android.tv.settings.R;
 import com.android.tv.settings.util.UriUtils;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Gets the list of browse headers and browse items.
  */
-public class AppsBrowseInfo extends BrowseInfoBase implements ApplicationsState.Callbacks {
+public class AppsBrowseInfo extends BrowseInfoBase {
+
+    private static final String TAG = "AppsBrowseInfo";
 
     private static final int DOWNLOADED_ID = 0;
     private static final int SYSTEM_ID = 1;
@@ -49,26 +55,96 @@ public class AppsBrowseInfo extends BrowseInfoBase implements ApplicationsState.
     private final Context mContext;
     private final HashMap<String, Integer> mAppItemIdList;
     private final ApplicationsState mApplicationsState;
-    private final ApplicationsState.Session mSession;
+    private final ApplicationsState.Session mSessionSystem;
+    private final ApplicationsState.AppFilter mFilterSystem;
+    private final ApplicationsState.Session mSessionDownloaded;
+    private final ApplicationsState.AppFilter mFilterDownloaded;
+    private final ApplicationsState.Session mSessionRunning;
+    private final ApplicationsState.AppFilter mFilterRunning;
     private int mNextItemId;
 
-    static class AppInfoComparator implements Comparator<AppInfo> {
+    private final Handler mHandler = new Handler();
+    private final Map<ArrayObjectAdapter,
+        ArrayList<ApplicationsState.AppEntry>> mUpdateMap = new ArrayMap<>(3);
+    private long mRunAt = Long.MIN_VALUE;
+    private final Runnable mUpdateRunnable = new Runnable() {
         @Override
-        public int compare(AppInfo o1, AppInfo o2) {
-            return o1.getName().compareToIgnoreCase(o2.getName());
+        public void run() {
+            for (final ArrayObjectAdapter adapter : mUpdateMap.keySet()) {
+                final ArrayList<ApplicationsState.AppEntry> entries = mUpdateMap.get(adapter);
+                updateAppListInternal(adapter, entries);
+            }
+            mUpdateMap.clear();
+            mRunAt = 0;
         }
-    }
+    };
 
-    AppsBrowseInfo(Activity context) {
+    AppsBrowseInfo(Activity context, String volumeUuid, String volumeDescription) {
         mContext = context;
         mAppItemIdList = new HashMap<>();
         mApplicationsState = ApplicationsState.getInstance(context.getApplication());
-        mSession = mApplicationsState.newSession(this);
-        mNextItemId = 0;
         mRows.put(SYSTEM_ID, new ArrayObjectAdapter());
         mRows.put(DOWNLOADED_ID, new ArrayObjectAdapter());
         mRows.put(RUNNING_ID, new ArrayObjectAdapter());
-        updateAppList();
+
+        // The UUID of internal storage is null, so we check if there's a volume name to see if we
+        // should only be showing the apps on the internal storage or all apps.
+        if (!TextUtils.isEmpty(volumeUuid) || !TextUtils.isEmpty(volumeDescription)) {
+            ApplicationsState.AppFilter volumeFilter =
+                    new ApplicationsState.VolumeFilter(volumeUuid);
+
+            mFilterSystem =
+                    new ApplicationsState.CompoundFilter(FILTER_SYSTEM, volumeFilter);
+            mFilterDownloaded =
+                    new ApplicationsState.CompoundFilter(FILTER_DOWNLOADED, volumeFilter);
+            mFilterRunning =
+                    new ApplicationsState.CompoundFilter(FILTER_RUNNING, volumeFilter);
+        } else {
+            mFilterSystem = FILTER_SYSTEM;
+            mFilterDownloaded = FILTER_DOWNLOADED;
+            mFilterRunning = FILTER_RUNNING;
+        }
+
+        mSessionSystem = mApplicationsState.newSession(new RowUpdateCallbacks() {
+            @Override
+            protected void doRebuild() {
+                rebuildSystem();
+            }
+
+            @Override
+            public void onRebuildComplete(ArrayList<ApplicationsState.AppEntry> apps) {
+                updateAppList(mRows.get(SYSTEM_ID), apps);
+            }
+        });
+        rebuildSystem();
+
+        mSessionDownloaded = mApplicationsState.newSession(new RowUpdateCallbacks() {
+            @Override
+            protected void doRebuild() {
+                rebuildDownloaded();
+            }
+
+            @Override
+            public void onRebuildComplete(ArrayList<ApplicationsState.AppEntry> apps) {
+                updateAppList(mRows.get(DOWNLOADED_ID), apps);
+            }
+        });
+        rebuildDownloaded();
+
+        mSessionRunning = mApplicationsState.newSession(new RowUpdateCallbacks() {
+            @Override
+            protected void doRebuild() {
+                rebuildRunning();
+            }
+
+            @Override
+            public void onRebuildComplete(ArrayList<ApplicationsState.AppEntry> apps) {
+                updateAppList(mRows.get(RUNNING_ID), apps);
+            }
+        });
+        rebuildRunning();
+
+        mNextItemId = 0;
         final ArrayObjectAdapter permissionsAdapter = new ArrayObjectAdapter();
         // TODO: different icon
         final MenuItem permissionsItem = new MenuItem.Builder()
@@ -80,84 +156,78 @@ public class AppsBrowseInfo extends BrowseInfoBase implements ApplicationsState.
         mRows.put(PERMISSIONS_ID, permissionsAdapter);
     }
 
-    @Override
-    public void onRunningStateChanged(boolean running) {
+    private void rebuildSystem() {
+        ArrayList<ApplicationsState.AppEntry> apps =
+            mSessionSystem.rebuild(mFilterSystem, ApplicationsState.ALPHA_COMPARATOR);
+        if (apps != null) {
+            updateAppList(mRows.get(SYSTEM_ID), apps);
+        }
     }
 
-    @Override
-    public void onLauncherInfoChanged() {
+    private void rebuildDownloaded() {
+        ArrayList<ApplicationsState.AppEntry> apps =
+                mSessionDownloaded.rebuild(mFilterDownloaded, ApplicationsState.ALPHA_COMPARATOR);
+        if (apps != null) {
+            updateAppList(mRows.get(DOWNLOADED_ID), apps);
+        }
     }
 
-    @Override
-    public void onLoadEntriesCompleted() {
+    private void rebuildRunning() {
+        ArrayList<ApplicationsState.AppEntry> apps =
+                mSessionRunning.rebuild(mFilterRunning, ApplicationsState.ALPHA_COMPARATOR);
+        if (apps != null) {
+            updateAppList(mRows.get(RUNNING_ID), apps);
+        }
     }
 
-    @Override
-    public void onPackageListChanged() {
-        updateAppList();
-    }
+    private void updateAppList(ArrayObjectAdapter row,
+            ArrayList<ApplicationsState.AppEntry> entries) {
+        mUpdateMap.put(row, entries);
 
-    @Override
-    public void onPackageIconChanged() {
-    }
-
-    @Override
-    public void onPackageSizeChanged(String packageName) {
-    }
-
-    @Override
-    public void onAllSizesComputed() {
-        updateAppList();
-    }
-
-    @Override
-    public void onRebuildComplete(ArrayList<ApplicationsState.AppEntry> apps) {
-    }
-
-    private void updateAppList() {
-        ArrayList<ApplicationsState.AppEntry> appEntries = mSession.getAllApps();
-        if (appEntries != null) {
-            ArrayList<AppInfo> appInfos = new ArrayList<AppInfo>(appEntries.size());
-            for (int i = 0, size = appEntries.size(); i < size; i++) {
-                appInfos.add(new AppInfo(mContext, appEntries.get(i)));
+        // We can get spammed with updates, so coalesce them to reduce jank and flicker
+        if (mRunAt == Long.MIN_VALUE) {
+            // First run, no delay
+            mHandler.removeCallbacks(mUpdateRunnable);
+            mHandler.post(mUpdateRunnable);
+        } else {
+            if (mRunAt == 0) {
+                mRunAt = SystemClock.uptimeMillis() + 1000;
             }
+            int delay = (int) (mRunAt - SystemClock.uptimeMillis());
+            delay = delay < 0 ? 0 : delay;
 
-            Collections.sort(appInfos, new AppInfoComparator());
+            mHandler.removeCallbacks(mUpdateRunnable);
+            mHandler.postDelayed(mUpdateRunnable, delay);
+        }
+    }
 
-            ArrayObjectAdapter systemRow = mRows.get(SYSTEM_ID);
-            ArrayObjectAdapter downloadedRow = mRows.get(DOWNLOADED_ID);
-            ArrayObjectAdapter runningRow = mRows.get(RUNNING_ID);
-            systemRow.clear();
-            downloadedRow.clear();
-            runningRow.clear();
-            for (int i = 0, size = appInfos.size(); i < size; i++) {
-                AppInfo info = appInfos.get(i);
-                String packageName = info.getPackageName();
-                Integer itemId = mAppItemIdList.get(packageName);
-                if (itemId == null) {
-                    itemId = mNextItemId++;
-                    mAppItemIdList.put(packageName, itemId);
-                }
+    private void updateAppListInternal(ArrayObjectAdapter row,
+            ArrayList<ApplicationsState.AppEntry> entries) {
+        if (entries != null) {
+            row.clear();
 
-                MenuItem menuItem = new MenuItem.Builder()
-                        .id(itemId)
-                        .title(info.getName())
-                        .description(info.getSize())
-                        .imageUri(getAppIconUri(mContext, info))
-                        .intent(AppManagementActivity.getLaunchIntent(info.getPackageName()))
-                        .build();
-
-                if (info.isSystemApp()) {
-                    systemRow.add(menuItem);
-                } else {
-                    downloadedRow.add(menuItem);
-                }
-
-                if (!info.isStopped()) {
-                    runningRow.add(menuItem);
-                }
+            for (final ApplicationsState.AppEntry entry : entries) {
+                row.add(getMenuItemForApp(entry));
             }
         }
+    }
+
+    private MenuItem getMenuItemForApp(ApplicationsState.AppEntry entry) {
+        final AppInfo info = new AppInfo(mContext, entry);
+        final String packageName = info.getPackageName();
+        Integer itemId = mAppItemIdList.get(packageName);
+        if (itemId == null) {
+            itemId = mNextItemId++;
+            mAppItemIdList.put(packageName, itemId);
+        }
+
+        return new MenuItem.Builder()
+                .id(itemId)
+                .title(info.getName())
+                .description(info.getSize())
+                .imageUri(getAppIconUri(mContext, info))
+                .intent(AppManagementActivity.getLaunchIntent(info.getPackageName()))
+                .build();
     }
 
     private void loadBrowseHeaders() {
@@ -170,7 +240,9 @@ public class AppsBrowseInfo extends BrowseInfoBase implements ApplicationsState.
     }
 
     void init() {
-        mSession.resume();
+        mSessionSystem.resume();
+        mSessionDownloaded.resume();
+        mSessionRunning.resume();
         loadBrowseHeaders();
     }
 
@@ -199,4 +271,80 @@ public class AppsBrowseInfo extends BrowseInfoBase implements ApplicationsState.
         }
         return iconUri;
     }
+
+    private abstract class RowUpdateCallbacks implements ApplicationsState.Callbacks {
+
+        protected abstract void doRebuild();
+
+        @Override
+        public void onRunningStateChanged(boolean running) {
+            doRebuild();
+        }
+
+        @Override
+        public void onPackageListChanged() {
+            doRebuild();
+        }
+
+        @Override
+        public void onPackageIconChanged() {
+            doRebuild();
+        }
+
+        @Override
+        public void onPackageSizeChanged(String packageName) {
+            doRebuild();
+        }
+
+        @Override
+        public void onAllSizesComputed() {
+            doRebuild();
+        }
+
+        @Override
+        public void onLauncherInfoChanged() {
+            doRebuild();
+        }
+
+        @Override
+        public void onLoadEntriesCompleted() {
+            doRebuild();
+        }
+    }
+
+    private static final ApplicationsState.AppFilter FILTER_SYSTEM =
+            new ApplicationsState.AppFilter() {
+
+                @Override
+                public void init() {}
+
+                @Override
+                public boolean filterApp(ApplicationsState.AppEntry info) {
+                    return (info.info.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
+                }
+            };
+
+    private static final ApplicationsState.AppFilter FILTER_DOWNLOADED =
+            new ApplicationsState.AppFilter() {
+
+                @Override
+                public void init() {}
+
+                @Override
+                public boolean filterApp(ApplicationsState.AppEntry info) {
+                    return (info.info.flags & ApplicationInfo.FLAG_SYSTEM) == 0;
+                }
+            };
+
+    private static final ApplicationsState.AppFilter FILTER_RUNNING =
+            new ApplicationsState.AppFilter() {
+
+                @Override
+                public void init() {}
+
+                @Override
+                public boolean filterApp(ApplicationsState.AppEntry info) {
+                    return (info.info.flags & ApplicationInfo.FLAG_STOPPED) == 0;
+                }
+            };
 }
