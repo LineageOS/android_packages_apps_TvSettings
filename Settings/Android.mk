6ff71f0728d56f8377e69797edd916779b80a7eb
Yvonne Wong <ywong@cyngn.com>
2015-09-30 03:36:01 -0700

    WIP - Switch TvSettings to use CMSettings
    Issue-Id: CYNGNOS-831
    Change-Id: I6383f9e6f8930c29a4a8b52858fb7fe369d53377

--- Settings/Android.mk
+++ Settings/Android.mk
@@ -26,7 +26,8 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v7-recyclerview \
     android-support-v13 \
-    android-support-v17-leanback
+    android-support-v17-leanback \
+    org.cyanogenmod.platform.sdk
 
 LOCAL_RESOURCE_DIR := \
     $(TOP)/frameworks/support/v17/leanback/res \
