07ff05b44ad2859b80f306fb73edf7765e07948c
Tony Mantler <nicoya@google.com>
2015-12-02 10:47:45 -0800

    Move stub file to correct package
    Also fix some find/replace errors.
    
    Change-Id: I2834e01a0dba72e3b46e0dd7e0d642bc2483b7bd

--- Settings/src/com/android/tv/settings/ResourcesUsedByLauncher.java
+++ Settings/src/com/android/tv/settings/ResourcesUsedByLauncher.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (theR.drawable.License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on anR.drawable.AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
 
-import android.support.annotation.Keep;
+package com.android.tv.settings;
 
-import com.android.tv.settings.R;
+import android.support.annotation.Keep;
 
 /**
  * This class keeps a reference to the drawables used by LeanbackLauncher
