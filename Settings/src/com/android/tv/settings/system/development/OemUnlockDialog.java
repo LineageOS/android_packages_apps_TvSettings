29e0208b8dc28b4180e8b2c6e5426bb61355cd34
Tony Mantler <nicoya@google.com>
2015-12-04 11:23:47 -0800

    Convert DeveloperOptionsActivity to new prefs fragment
    Change-Id: I954e244739ed775f73c1cb2ab65331e81502717b

---
+++ Settings/src/com/android/tv/settings/system/development/OemUnlockDialog.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.tv.settings.system.development;
+
+import android.os.Bundle;
+import android.support.annotation.Keep;
+import android.support.annotation.NonNull;
+import android.support.v17.leanback.app.GuidedStepFragment;
+import android.support.v17.leanback.widget.GuidanceStylist;
+import android.support.v17.leanback.widget.GuidedAction;
+
+import com.android.tv.settings.R;
+
+import java.util.List;
+
+@Keep
+public class OemUnlockDialog extends GuidedStepFragment {
+
+    public interface Callback {
+        void onOemUnlockConfirm();
+    }
+
+    @NonNull
+    @Override
+    public GuidanceStylist.Guidance onCreateGuidance(Bundle savedInstanceState) {
+        return new GuidanceStylist.Guidance(
+                getString(R.string.confirm_enable_oem_unlock_title),
+                getString(R.string.confirm_enable_oem_unlock_text),
+                null,
+                null);
+    }
+
+    @Override
+    public void onCreateActions(@NonNull List<GuidedAction> actions, Bundle savedInstanceState) {
+        actions.add(new GuidedAction.Builder()
+                .constructOK(getContext())
+                .title(getString(R.string.device_apps_app_management_enable))
+                .build());
+        actions.add(new GuidedAction.Builder().constructCancel(getContext()).build());
+    }
+
+    @Override
+    public void onGuidedActionClicked(GuidedAction action) {
+        if (action.getId() == GuidedAction.ACTION_ID_OK) {
+            ((Callback) getTargetFragment()).onOemUnlockConfirm();
+            getFragmentManager().popBackStack();
+        } else {
+            getFragmentManager().popBackStack();
+        }
+    }
+}
