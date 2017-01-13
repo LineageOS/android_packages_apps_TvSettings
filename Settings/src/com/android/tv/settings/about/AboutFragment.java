a192ca0ce0bf9f3715181216e4cf0fadbdd876cc
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-14 02:18:58 +0800

    Settings: Add intent to launch LineageOS platlogo upon multiple taps
    Change-Id: I8e06642944d24581c0932c9964f2d9615bfcb832

--- Settings/src/com/android/tv/settings/about/AboutFragment.java
+++ Settings/src/com/android/tv/settings/about/AboutFragment.java
@@ -256,6 +256,7 @@ public class AboutFragment extends LeanbackPreferenceFragment implements
     public boolean onPreferenceTreeClick(Preference preference) {
         switch (preference.getKey()) {
             case KEY_FIRMWARE_VERSION:
+            case KEY_ROM_VERSION:
                 System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
                 mHits[mHits.length - 1] = SystemClock.uptimeMillis();
                 if (mHits[0] >= (SystemClock.uptimeMillis() - 500)) {
@@ -265,6 +266,7 @@ public class AboutFragment extends LeanbackPreferenceFragment implements
                     }
 
                     Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.putExtra("is_lineage", preference.getKey().equals(KEY_ROM_VERSION));
                     intent.setClassName("android",
                             com.android.internal.app.PlatLogoActivity.class.getName());
                     try {
