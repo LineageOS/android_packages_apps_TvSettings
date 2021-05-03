4be539b74d811442fc3d83a54a20471d345ec35e
Zhizhi Liu <zhizhiliu@google.com>
2021-05-03 06:11:57 +0000

    Create TvSettings SDK library.
    Skeleton code of TvSettings SDK library to provide API level classes.
    
    Bug: 184803758
    CTS-Coverage-Bug: 187659818
    Test: Manual
    Change-Id: I4a62eb7a2ceaed77cf8a847531e009c7dc353625

---
+++ SettingsAPI/java/com/android/tv/settings/library/ManagerUtil.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tv.settings.library;
+
+import android.annotation.SystemApi;
+
+/**
+ * @hide
+ * Provide constants and utility methods.
+ */
+@SystemApi
+public final class ManagerUtil {
+    public static final int STATE_NETWORK_MAIN = 0;
+    public static final int STATE_WIFI_DETAILS = 1;
+    public static final int STATE_DEVICE_MAIN = 2;
+    public static final int STATE_APPS = 3;
+    public static final int STATE_ALL_APPS = 4;
+    public static final int STATE_APP_MANAGEMENT = 5;
+
+    public static final String INFO_INTENT = "intent";
+    public static final String INFO_NEXT_STATE = "next_state";
+    public static final String INFO_WIFI_SIGNAL_LEVEL = "wifi_signal_level";
+    public static final String INFO_COLLAPSE = "collapse";
+
+    private ManagerUtil() {
+    }
+
+    static byte getChecked(boolean checked) {
+        return checked ? (byte) 2 : (byte) 1;
+    }
+
+    static byte getVisible(boolean visible) {
+        return visible ? (byte) 2 : (byte) 1;
+    }
+
+    /**
+     * @hide
+     * Return whether the preference is checked.
+     * 0 : not updated, 1 : unchecked, 2 : checked
+     */
+    @SystemApi
+    public static boolean isChecked(PreferenceCompat pref) {
+        return pref.getChecked() == 2 ? true : false;
+    }
+
+    /**
+     * @hide
+     * Return whether the preference is visible.
+     * 0 : not updated, 1 : invisible, 2 : visible
+     */
+    @SystemApi
+    public static boolean isVisible(PreferenceCompat pref) {
+        return pref.getVisible() == 2 ? true : false;
+    }
+}
