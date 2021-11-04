1669ed056d095566045f708badc404344cee23e3
Aaron Kling <webgeek1234@gmail.com>
2021-11-03 23:39:46 -0500

    Add support for Nvidia convertible USB port
    The Shield Android TV models darcy and mdarcy have a USB A port
    that converts between host and device mode for adb.
    
    Change-Id: Iee07ccdfccc1f29976f5c69db45c16b09c523673

--- Settings/src/com/android/tv/settings/system/development/DevelopmentFragment.java
+++ Settings/src/com/android/tv/settings/system/development/DevelopmentFragment.java
@@ -88,6 +88,7 @@ import com.android.tv.settings.library.system.development.audio.AudioDebug;
 import com.android.tv.settings.library.system.development.audio.AudioMetrics;
 import com.android.tv.settings.library.system.development.audio.AudioReaderException;
 import com.android.tv.settings.library.overlay.FlavorUtils;
+import com.android.tv.settings.system.ShieldMtpActivity;
 
 import java.net.NetworkInterface;
 import java.net.InetAddress;
@@ -192,6 +193,9 @@ public class DevelopmentFragment extends SettingsPreferenceFragment
     private static final String STATE_SHOWING_DIALOG_KEY = "showing_dialog_key";
 
     private static final String TOGGLE_ADB_WIRELESS_KEY = "toggle_adb_wireless";
+
+    private static final String SETTINGS_PKG = "com.android.tv.settings";
+    private static final String SHIELD_MTP_CLASS = ".system.ShieldMtpActivity";
 
     private String mPendingDialogKey;
 
@@ -1657,6 +1661,14 @@ public class DevelopmentFragment extends SettingsPreferenceFragment
 
     @Override
     public void onEnableAdbConfirm() {
+        if (getResources().getBoolean(R.bool.has_convertible_port)) {
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName(SETTINGS_PKG, SHIELD_MTP_CLASS));
+            intent.putExtra(ShieldMtpActivity.CALLER, true);
+            intent.putExtra(ShieldMtpActivity.PAGE_NUMBER,
+                    ShieldMtpActivity.USB_DEBUG_INFO_PAGE);
+            startActivity(intent);
+        }
         Settings.Global.putInt(mContentResolver, Settings.Global.ADB_ENABLED, 1);
         mEnableAdb.setChecked(true);
         updateVerifyAppsOverUsbOptions();
@@ -1702,12 +1714,22 @@ public class DevelopmentFragment extends SettingsPreferenceFragment
         } else if (preference == mBugreport) {
             captureBugReport(this.getActivity());
         } else if (preference == mEnableAdb) {
-            if (mEnableAdb.isChecked()) {
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName(SETTINGS_PKG, SHIELD_MTP_CLASS));
+            intent.putExtra(ShieldMtpActivity.CALLER, true);
+            if (ShieldMtpActivity.isConvPortBusy(getActivity())) {
+                intent.putExtra(ShieldMtpActivity.PAGE_NUMBER,
+                        ShieldMtpActivity.PORT_BUSY_PAGE);
+                startActivity(intent);
+            } else if (mEnableAdb.isChecked()) {
                 // Pass to super to launch the dialog, then uncheck until the dialog
                 // result comes back
                 super.onPreferenceTreeClick(preference);
                 mEnableAdb.setChecked(false);
             } else {
+                if (getResources().getBoolean(R.bool.has_convertible_port)) {
+                    ShieldMtpActivity.setPortMode(getActivity(), ShieldMtpActivity.HOST_MODE, true);
+                }
                 Settings.Global.putInt(mContentResolver, Settings.Global.ADB_ENABLED, 0);
                 mVerifyAppsOverUsb.setEnabled(false);
                 mVerifyAppsOverUsb.setChecked(false);
