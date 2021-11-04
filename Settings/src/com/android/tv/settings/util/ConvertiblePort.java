b78e0d1e48ae74253f12a2ec2259e24ad66349a5
Aaron Kling <webgeek1234@gmail.com>
2021-11-03 23:39:46 -0500

    Add support for Nvidia convertible usb port
    The shield android tv models darcy and mdarcy have a usb A port
    that converts between host and device mode for adb.
    
    Change-Id: Iee07ccdfccc1f29976f5c69db45c16b09c523673

---
+++ Settings/src/com/android/tv/settings/util/ConvertiblePort.java
@@ -0,0 +1,41 @@
+package com.android.tv.settings.util;
+
+import android.content.Context;
+import android.os.RemoteException;
+import android.os.SystemProperties;
+import android.util.Log;
+import com.android.tv.settings.R;
+
+public final class ConvertiblePort {
+    private static final String TAG = "ConvertiblePortUtil";
+    public static final String USB_PORT_MODE = "persist.vendor.convertible.usb.mode";
+
+    private ConvertiblePort() {
+    }
+
+    public static final String getConvertiblePortSetting(Context context) {
+        if (!context.getResources().getBoolean(R.bool.has_convertible_port)) {
+            Log.d(TAG, "getConvertiblePortSetting: Non-valid platform");
+            return "";
+        }
+        try {
+            return SystemProperties.get(USB_PORT_MODE);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Failed to get port mode", e);
+            return "";
+        }
+    }
+
+    public static final int setConvertiblePortSetting(Context context, String mode) {
+        if (!context.getResources().getBoolean(R.bool.has_convertible_port)) {
+            Log.d(TAG, "setConvertiblePortSetting: Non-valid platform");
+            return -1;
+        }
+        try {
+            return SystemProperties.set(USB_PORT_MODE, mode);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Failed to set port mode", e);
+            return -1;
+        }
+    }
+}
