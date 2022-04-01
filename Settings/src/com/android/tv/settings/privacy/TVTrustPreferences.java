165d3577ae5aa4ab7ae9e50405a1357f5ac4243b
Bruno Martins <bgcngm@gmail.com>
2022-04-01 09:19:50 +0100

    TVTrustPreferences: Remove unused isTelephony method
    Change-Id: I68b7a934758055b48194c9d96851d3c2354781ea

--- Settings/src/com/android/tv/settings/privacy/TVTrustPreferences.java
+++ Settings/src/com/android/tv/settings/privacy/TVTrustPreferences.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2020 The LineageOS Project
+ * Copyright (C) 2021-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@ package com.android.tv.settings.privacy;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.Settings;
 
@@ -217,12 +216,6 @@ public class TVTrustPreferences extends SettingsPreferenceFragment {
             mInterface.removeNotificationForFeature(feature);
         }
         return success;
-    }
-
-
-    private boolean isTelephony() {
-        PackageManager pm = getContext().getPackageManager();
-        return pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
     }
 
     @Override
