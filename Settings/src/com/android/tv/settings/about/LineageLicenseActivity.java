22b5b4abeb3717a23ef60869203e138d40ec0c17
Bruno Martins <bgcngm@gmail.com>
2022-04-01 11:49:49 +0100

    LegalFragment: Add LineageOS legal info
    Open up the LineageOS legal info in the browser,
    just as we do in the regular Settings app.
    
    Change-Id: Ic420ddedc0c5fc89af648473ccab7b47214a46b2

---
+++ Settings/src/com/android/tv/settings/about/LineageLicenseActivity.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tv.settings.about;
+
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.util.Log;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.android.tv.settings.R;
+
+public class LineageLicenseActivity extends FragmentActivity {
+    private static final String TAG = "LineageLicenseActivity";
+
+    private static final String PROPERTY_LINEAGE_LICENSE_URL = "ro.lineagelegal.url";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = Uri.parse(SystemProperties.get(PROPERTY_LINEAGE_LICENSE_URL));
+        intent.setDataAndType(uri, "text/html");
+        intent.putExtra(Intent.EXTRA_TITLE, getString(R.string.about_legal_license));
+        if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme())) {
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setPackage("com.android.htmlviewer");
+
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Log.e(TAG, "Failed to find viewer", e);
+        }
+        finish();
+    }
+}
