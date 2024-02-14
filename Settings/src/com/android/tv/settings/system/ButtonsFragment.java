9971ec7a7777836f75eb6cf41e4bc216a51d844c
Timi Rautamäki <timi.rautamaki@gmail.com>
2024-02-14 15:04:40 +0000

    TvSettings: implement long press power settings
    Change-Id: Ic7d9e23f8167b70ca8d24468580b6bf5db05f264

--- Settings/src/com/android/tv/settings/system/ButtonsFragment.java
+++ Settings/src/com/android/tv/settings/system/ButtonsFragment.java
@@ -20,8 +20,11 @@ import android.app.tvsettings.TvSettingsEnums;
 import android.os.Bundle;
 
 import androidx.annotation.Keep;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
 
 import com.android.tv.settings.R;
+import com.android.tv.settings.RadioPreference;
 import com.android.tv.settings.SettingsPreferenceFragment;
 
 import org.lineageos.internal.logging.LineageMetricsLogger;
@@ -31,6 +34,16 @@ import org.lineageos.internal.logging.LineageMetricsLogger;
  */
 @Keep
 public class ButtonsFragment extends SettingsPreferenceFragment {
+    private static final String KEY_GESTURE_POWER_MENU_LONG_PRESS_FOR_POWER_MENU =
+            "gesture_power_menu_long_press_for_power_menu";
+    private static final String KEY_GESTURE_POWER_MENU_LONG_PRESS_FOR_ASSISTANT =
+            "gesture_power_menu_long_press_for_assistant";
+    private static final String KEY_GESTURE_POWER_MENU_LONG_PRESS_GROUP =
+            "gesture_power_menu_long_press_category";
+
+    private PreferenceGroup mPowerKeyLongPressGroup;
+    private RadioPreference mPowerKeyLongPressForPowerMenu;
+    private RadioPreference mPowerKeyLongPressForAssistant;
 
     public static ButtonsFragment newInstance() {
         return new ButtonsFragment();
@@ -39,5 +52,38 @@ public class ButtonsFragment extends SettingsPreferenceFragment {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.buttons, null);
+
+        mPowerKeyLongPressForPowerMenu =
+                findPreference(KEY_GESTURE_POWER_MENU_LONG_PRESS_FOR_POWER_MENU);
+        mPowerKeyLongPressForAssistant =
+                findPreference(KEY_GESTURE_POWER_MENU_LONG_PRESS_FOR_ASSISTANT);
+        mPowerKeyLongPressGroup = findPreference(KEY_GESTURE_POWER_MENU_LONG_PRESS_GROUP);
+
+        // Set the default value
+        if (PowerMenuSettingsUtils.isLongPressPowerForAssistantEnabled(getContext())) {
+            mPowerKeyLongPressForPowerMenu.setChecked(false);
+            mPowerKeyLongPressForAssistant.setChecked(true);
+        } else {
+            mPowerKeyLongPressForPowerMenu.setChecked(true);
+            mPowerKeyLongPressForAssistant.setChecked(false);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference.getKey() == null) {
+            return super.onPreferenceTreeClick(preference);
+        }
+        switch (preference.getKey()) {
+            case KEY_GESTURE_POWER_MENU_LONG_PRESS_FOR_POWER_MENU:
+                PowerMenuSettingsUtils.setLongPressPowerForPowerMenu(getContext());
+                mPowerKeyLongPressForPowerMenu.clearOtherRadioPreferences(mPowerKeyLongPressGroup);
+                return true;
+            case KEY_GESTURE_POWER_MENU_LONG_PRESS_FOR_ASSISTANT:
+                PowerMenuSettingsUtils.setLongPressPowerForAssistant(getContext());
+                mPowerKeyLongPressForAssistant.clearOtherRadioPreferences(mPowerKeyLongPressGroup);
+                return true;
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 }
