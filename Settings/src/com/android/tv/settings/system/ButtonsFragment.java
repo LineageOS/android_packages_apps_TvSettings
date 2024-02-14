b7940ccda12c4c5f1dd9b29a85ca9ad93d51ebb7
Timi Rautamäki <timi.rautamaki@gmail.com>
2024-02-14 15:04:40 +0000

    TvSettings: implement long press power settings
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Change-Id: Ic7d9e23f8167b70ca8d24468580b6bf5db05f264

--- Settings/src/com/android/tv/settings/system/ButtonsFragment.java
+++ Settings/src/com/android/tv/settings/system/ButtonsFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2021-2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import android.app.tvsettings.TvSettingsEnums;
 import android.os.Bundle;
 
 import androidx.annotation.Keep;
+import androidx.preference.ListPreference;
 
 import com.android.tv.settings.R;
 import com.android.tv.settings.SettingsPreferenceFragment;
@@ -31,6 +32,13 @@ import org.lineageos.internal.logging.LineageMetricsLogger;
  */
 @Keep
 public class ButtonsFragment extends SettingsPreferenceFragment {
+    private static final String KEY_POWER_BUTTON_LONG_PRESS_ACTION =
+            "power_button_long_press_action";
+
+    private static final int LONG_PRESS_POWER_BUTTON_FOR_ASSISTANT = 1;
+    private static final int LONG_PRESS_POWER_BUTTON_FOR_POWER_MENU = 0;
+
+    private ListPreference mPowerButtonLongPressAction;
 
     public static ButtonsFragment newInstance() {
         return new ButtonsFragment();
@@ -39,5 +47,24 @@ public class ButtonsFragment extends SettingsPreferenceFragment {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.buttons, null);
+
+        mPowerButtonLongPressAction = findPreference(KEY_POWER_BUTTON_LONG_PRESS_ACTION);
+        if (PowerMenuSettingsUtils.isLongPressPowerSettingAvailable(getContext())) {
+            mPowerButtonLongPressAction.setOnPreferenceChangeListener(
+                (preference, newValue) -> {
+                    int action = Integer.parseInt((String) newValue);
+                    switch (action) {
+                        case LONG_PRESS_POWER_BUTTON_FOR_ASSISTANT:
+                            PowerMenuSettingsUtils.setLongPressPowerForAssistant(getContext());
+                            break;
+                        case LONG_PRESS_POWER_BUTTON_FOR_POWER_MENU:
+                            PowerMenuSettingsUtils.setLongPressPowerForPowerMenu(getContext());
+                            break;
+                    }
+                    return true;
+                });
+        } else {
+            getPreferenceScreen().removePreference(mPowerButtonLongPressAction);
+        }
     }
 }
