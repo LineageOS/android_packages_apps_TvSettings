bd041c15ffb3a8390838be171afb3ea5f97d54e4
Aaron Kling <webgeek1234@gmail.com>
2024-03-06 16:10:47 -0600

    Allow disabling Frame Rate settings
    Some graphics implementations do not support this api and should not
    show users the options.
    
    Change-Id: If90613adfaf7e1e32eca080065e98eabe3246057
    (cherry picked from commit 77a2db752ec1e20d9b16372359b05d5cab820ecb)

--- Settings/src/com/android/tv/settings/device/displaysound/DisplaySoundFragment.kt
+++ Settings/src/com/android/tv/settings/device/displaysound/DisplaySoundFragment.kt
@@ -115,6 +115,11 @@ open class DisplaySoundFragment : SettingsPreferenceFragment(), DisplayManager.D
         findPreference<TwoStatePreference>(KEY_SOUND_EFFECTS)?.isChecked = soundEffectsEnabled
         updateCecPreference()
 
+
+        if (!resources.getBoolean(R.bool.enable_framerate_config)) {
+            removePreference(findPreference(KEY_FRAMERATE))
+        }
+
         mCurrentDeviceName = DeviceUtils.getDeviceName(context)
         updateVolumeChangePreference()
 
@@ -272,6 +277,7 @@ open class DisplaySoundFragment : SettingsPreferenceFragment(), DisplayManager.D
         private const val KEY_DEFAULT_AUDIO_OUTPUT_SETTINGS_SLICE = "default_audio_output_settings"
         private const val KEY_RESOLUTION_TITLE = "resolution_selection"
         private const val KEY_DYNAMIC_RANGE = "match_content_dynamic_range"
+	private const val KEY_FRAMERATE = "match_content_frame_rate"
 
         private const val VOLUME_CHANGE = "volume_change"
 
