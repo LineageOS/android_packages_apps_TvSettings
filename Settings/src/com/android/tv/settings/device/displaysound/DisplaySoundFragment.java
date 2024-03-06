b4881f7ac704398b4d5f0d13e29dde4d302249c3
Aaron Kling <webgeek1234@gmail.com>
2024-03-06 16:10:47 -0600

    Allow disabling Frame Rate settings
    Some graphics implementations do not support this api and should not
    show users the options.
    
    Change-Id: If90613adfaf7e1e32eca080065e98eabe3246057
    (cherry picked from commit 77a2db752ec1e20d9b16372359b05d5cab820ecb)

--- Settings/src/com/android/tv/settings/device/displaysound/DisplaySoundFragment.java
+++ Settings/src/com/android/tv/settings/device/displaysound/DisplaySoundFragment.java
@@ -61,6 +61,7 @@ public class DisplaySoundFragment extends SettingsPreferenceFragment implements
     private static final String KEY_CEC = "cec";
     private static final String KEY_DEFAULT_AUDIO_OUTPUT_SETTINGS_SLICE =
             "default_audio_output_settings";
+    private static final String KEY_FRAMERATE = "match_content_frame_rate";
     private static final String KEY_RESOLUTION_TITLE = "resolution_selection";
     private static final String KEY_DYNAMIC_RANGE = "match_content_dynamic_range";
 
@@ -107,6 +108,10 @@ public class DisplaySoundFragment extends SettingsPreferenceFragment implements
         soundPref.setChecked(getSoundEffectsEnabled());
         updateCecPreference();
         updateDefaultAudioOutputSettings();
+
+        if (!getResources().getBoolean(R.bool.enable_framerate_config)) {
+            removePreference(findPreference(KEY_FRAMERATE));
+        }
 
         mDisplayManager = getDisplayManager();
         Display display = mDisplayManager.getDisplay(Display.DEFAULT_DISPLAY);
