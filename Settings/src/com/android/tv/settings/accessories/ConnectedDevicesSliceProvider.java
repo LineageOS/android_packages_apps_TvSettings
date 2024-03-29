ad6db903d1f23cfc50474d6e75a7fc4996849bff
eric_kuang.rs <eric_kuang.rs@realtek.com>
2024-03-29 17:37:19 +0800

    Replace to the parent URL when Bluetooth device is null.
    Test: atest MainFragmentTest
    Bug: 317187207
    Change-Id: Ibdb74c8237c14357caa245a19f3cb806a07ace62

--- Settings/src/com/android/tv/settings/accessories/ConnectedDevicesSliceProvider.java
+++ Settings/src/com/android/tv/settings/accessories/ConnectedDevicesSliceProvider.java
@@ -178,6 +178,9 @@ public class ConnectedDevicesSliceProvider extends SliceProvider implements
         if (DEBUG) {
             Log.d(TAG, "onBindSlice: " + sliceUri);
         }
+        if (getBluetoothDevices().isEmpty()) {
+            sliceUri = ConnectedDevicesSliceUtils.GENERAL_SLICE_URI;
+        }
         StrictMode.ThreadPolicy oldPolicy = StrictMode.getThreadPolicy();
         try {
             // Prevent StrictMode from throwing on access to shared preferences.
