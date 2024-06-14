4fe179cec27e3f4e2a7dcc1a0960f5b54100decd
Achille Laroche <achille.laroche@sagemcom.com>
2024-06-14 14:28:22 +0200

    Check for invalid resource ID instead of null, this way the partner customization module doesn't crash on missing resource and permits partners to customize a few modules instead of all of them.
    Change-Id: I6813d34bff5db6ecda026e9fcd90ceb3fcf00cbc

--- Settings/src/com/android/tv/settings/customization/Partner.java
+++ Settings/src/com/android/tv/settings/customization/Partner.java
@@ -16,6 +16,8 @@
 
 package com.android.tv.settings.customization;
 
+import static androidx.core.content.res.ResourcesCompat.ID_NULL;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -88,7 +90,11 @@ public final class Partner {
             Log.i(TAG, "Partner customization resource reference is null");
             return null;
         }
-        return mResources.getIdentifier(name, defType, mPackage);
+        Integer id = mResources.getIdentifier(name, defType, mPackage);
+        if (id == null || id == ID_NULL) {
+            return null;
+        }
+        return id;
     }
 
     @Nullable
