fd02f84ba26cd5e9c923b7ea799189b719330cd4
Aaron Kling <webgeek1234@gmail.com>
2025-02-10 13:56:02 -0600

    TvSettings: Support two button mute
    Volume up and down to mute, for remotes that don't have a dedicated mute
    button.
    
    Change-Id: Ica4ea5490376a08201c3ec9a616bb1ef337a4401

--- Settings/src/com/android/tv/settings/system/ButtonsFragment.java
+++ Settings/src/com/android/tv/settings/system/ButtonsFragment.java
@@ -37,6 +37,9 @@ import org.lineageos.internal.logging.LineageMetricsLogger;
 public class ButtonsFragment extends SettingsPreferenceFragment {
     private static final String KEY_POWER_BUTTON_LONG_PRESS_ACTION =
             "power_button_long_press_action";
+    private static final String KEY_PRESS_VOLUME_UP_AND_DOWN_TO_MUTE =
+            "press_volume_up_and_down_to_mute";
+
 
     private static final int LONG_PRESS_POWER_BUTTON_FOR_ASSISTANT = 1;
     private static final int LONG_PRESS_POWER_BUTTON_FOR_POWER_MENU = 0;
@@ -74,6 +77,13 @@ public class ButtonsFragment extends SettingsPreferenceFragment {
         } else {
             getPreferenceScreen().removePreference(mPowerButtonLongPressAction);
         }
+
+        findPreference(KEY_PRESS_VOLUME_UP_AND_DOWN_TO_MUTE).setOnPreferenceChangeListener(
+            (preference, newValue) -> {
+                LineageSettings.System.putInt(context.getContentResolver(),
+                        LineageSettings.System.VOLUME_UP_AND_DOWN_MUTE, (Boolean) newValue ? 1 : 0);
+                return true;
+            });
     }
 
     private boolean hasAssistant() {
