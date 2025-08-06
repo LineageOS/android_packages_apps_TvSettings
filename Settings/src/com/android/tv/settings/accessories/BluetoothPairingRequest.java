8254d077bfdae72f4cf52ca08ad0f7378b0cf330
Brian Valcarcel <bval@google.com>
2025-08-06 16:11:17 -0700

    Revert "Move BluetoothConfirmation dialogs to FullScreenDialog in TwoPanel Mode"
    This reverts commit 539e7b0a674dfbd0a1cf125c7229da282649239f.
    
    Reason for revert: breaks build, needed to rebase first due to changing class name.
    
    Bug: 355462390
    Change-Id: Iaaa19c22015d503cee1dbc4f1441edfce8348f1f

--- Settings/src/com/android/tv/settings/accessories/BluetoothPairingRequest.java
+++ Settings/src/com/android/tv/settings/accessories/BluetoothPairingRequest.java
@@ -27,7 +27,6 @@ import android.util.Log;
 
 import com.android.settingslib.bluetooth.CachedBluetoothDevice;
 import com.android.settingslib.bluetooth.LocalBluetoothManager;
-import com.android.tv.settings.overlay.FlavorUtils;
 
 /**
  * BluetoothPairingRequest is a receiver for any Bluetooth pairing request. It
@@ -49,11 +48,6 @@ public final class BluetoothPairingRequest extends BroadcastReceiver {
             int type = intent.getIntExtra(BluetoothDevice.EXTRA_PAIRING_VARIANT,
                     BluetoothDevice.ERROR);
             Intent pairingIntent = new Intent();
-            if (FlavorUtils.isTwoPanel(context)) {
-                pairingIntent.setClass(context, BluetoothPairingFullScreenDialogActivity.class);
-            } else {
-                pairingIntent.setClass(context, BluetoothPairingDialog.class);
-            }
             pairingIntent.setClass(context, BluetoothPairingDialog.class);
             pairingIntent.putExtra(BluetoothDevice.EXTRA_DEVICE, device);
             pairingIntent.putExtra(BluetoothDevice.EXTRA_PAIRING_VARIANT, type);
