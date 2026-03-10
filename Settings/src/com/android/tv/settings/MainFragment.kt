0510119012634f987d1a7f78fda7c21a3635ed2b
junlu.jiang <junlu.jiang@amlogic.com>
2026-03-10 10:31:22 -0700

    TvSettings: Ethernet statue update [1/1]
    BUG=487561617
    PD#SWPL-249691
    
    Problem:
    onSlice()After the slice is loaded, the actual preferences are populated, but updateConnectivity() is not called, causing the network icon to remain at the XML default value (full Wi-Fi icon, empty summary).
    
    Solution:
    Add an explicit updateConnectivity() call at the end of onSlice(), after configurePreferences() inflates the real preferences, so the network state is immediately refreshed when the Slice is ready.
    
    Verify:
    android16 Ross
    
    Flag: EXEMPT BUGFIX
    Test: build and run
    Signed-off-by: junlu.jiang <junlu.jiang@amlogic.com>
    Cherrypick-From: https://tv-keystone-amlogic-review.googlesource.com/q/commit:ff5cb0edc9e0a0e69e7e313f177bb8f88c67e774
    Merged-In: Ie1a8ad81874e4391c93b7597fc5817dd9f61b244
    Change-Id: Ie1a8ad81874e4391c93b7597fc5817dd9f61b244

--- Settings/src/com/android/tv/settings/MainFragment.kt
+++ Settings/src/com/android/tv/settings/MainFragment.kt
@@ -209,6 +209,7 @@ open class MainFragment : PreferenceControllerFragment(),
         mSuggestions?.let {
             mSuggestionQuickSettingPrefsContainer!!.onSuggestionReady(it)
         }
+        updateConnectivity()
     }
 
     private fun configurePreferences() {
